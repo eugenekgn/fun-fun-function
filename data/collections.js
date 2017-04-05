1fd56b6404eae1853a8115d95a72fbdb51a119b1
Eugene Kagan <eugenekgn@gmail.com>
2017-04-04 20:43:16 -0400

    adding mapping example

---
+++ data/collections.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const animals = [
+  { name: 'Fluffykins', species: 'rabbit' },
+  { name: 'Cari', species: 'dog' },
+  { name: 'Hamilton', species: 'dog' },
+  { name: 'Harold', species: 'fish' },
+  { name: 'Ursula', species: 'cat' },
+  { name: 'Jimmy', species: 'fish' },
+];
+
+module.exports ={
+  animals
+};
---
+++ map.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const collections = require('./data/collections');
+
+// map - iterates through an array transforming values 
+
+const animalsCollection = collections.animals;
+
+const getNames = (a) => `${a.name} is a ${a.species}`;
+
+const names = animalsCollection.map(getNames);
+
+console.log(names);
